68a9d7d15c9a10cfe4d07f4a8f3d35ab11ac213d
Orkhan Mamedov <orkhan.mamedov@sravni.ru>
2022-02-18 10:05:14 +0300

    using external template files

---
+++ udemy-astakhov/lesson-4/webserfer.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  profile	= "${var.profile}"
+  region 	= "${var.region}"
+}
+
+resource "aws_instance" "terraform_arhel" {
+  count				= 1
+  ami				= "ami-0bf84c42e04519c85"
+  instance_type 		= "t3.micro"
+  vpc_security_group_ids 	= ["sg-d92b65bc"]
+  user_data 			= templatefile("./user-data.sh.tpl", {
+	f_name = "Orkhan",
+	l_name = "Mamedov",
+	names = ["vasya", "kolya", "petya", "john", "kate"]
+    })
+
+  tags = {
+    Name = "aws-tf-arhel"
+  }
+}
