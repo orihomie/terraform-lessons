00ed5cd94123df0015f6d8a092954f872c9d0af0
Orkhan Mamedov <orkhan.mamedov@sravni.ru>
2022-02-21 09:18:55 +0300

    dynamic vars

--- udemy-astakhov/lesson-5/dynamic-sec-group.tf
+++ udemy-astakhov/lesson-5/dynamic-sec-group.tf
@@ -4,24 +4,18 @@ provider "aws" {
 }
 
 resource "aws_security_group" "my_webserverl" {
-	name			= "WebServer Security Group"
+	name			= "Dynamic Security Group"
 	description		= "Security Group made by Terraform"
 
-	ingress {
-		from_port = 80
-		to_port = 80
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
+	dynamic "ingress" {
+		for_each = ["80", "443", "8080", "1541"]
+		content {
+			from_port = ingress.value
+			to_port = ingress.value
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
 	}
-
-	
-	ingress {
-		from_port = 443
-		to_port = 443
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
 	
 	egress {
 		from_port = 0
---
+++ udemy-astakhov/lesson-5/readme.md
@@ -0,0 +1,1 @@
+In this lesson I've learned how to use `dynamic` code word along with `for_each`and `content` to make dynamic variables for terraform to apply.
